d8b1904407fdb5b453bc61826acfdf0c41e11899
Lizzie <cooperedanielle@gmail.com>
2025-03-01 05:56:30 +0000

    commit

--- Main.java
+++ Main.java
@@ -6,13 +6,23 @@
 
 public class Main
 {
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) //method header
+	{//method body
 		System.out.println("My thoughts on the article are:");
+		System.out.println();
 		// Part 2: Invoking/calling method
-		
+		articleResponse();
 	}
 	
 	// Part 1: Creating method definition
+	//return type(void)  //method identifier(articleResponse)
+	public static void articleResponse()//method hearder
+	{//method body
+		System.out.println("Question 1: I believe that self-driving cars should prioritize pedestrians' safety because they are bystanders. Anyone getting into a self-driving car, or car in general, understands that they are putting their life at risk while in the motor vehicle and it is their responsibility to have control over the car. This means that despite the car being self-driving, the passenger should always be alert and have backup plans if anything were to go wrong. Therefore, the pedestrians, who aren't responsible for the car's activity, should have precedence.");
+		System.out.println();
+		System.out.println("Question 2: The cars algorithms should prioritize sensors that can detect pedestrians even if they aren't using legal means of crossing (i.e jaywalking). From this the car should know to slow or stop. Moreover, the sensors should be able to detect and avoid collisions with any other movement regardless of if they abide by legal conduct (i.e other cars). Only when these sensors are flawless, should we trust self-driving cars to operate without a person behind the wheel for emergency situations.");
+		System.out.println();
+		System.out.println("Question 3: The algorithm for self-driving cars should be conducted by experts who have broadly tested features and rake in additional algorithm ideas from everyday drivers who share their experiences and ways to handle situations.");
+	}
 	
 }
